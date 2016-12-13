23e15378dcb13d87dfb77efaa2306ad9e88ea060
wouter_baeyens <wouter.baeyens@student.khleuven.be>
2016-12-13 11:10:59 +0100

    Start Central Data Service

---
+++ src/main/java/com/mycompany/centraldataservice/DataContainer.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.centraldataservice;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.AUTO;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Wouter
+ */
+@Entity
+public class DataContainer {
+    
+    @Id
+    @GeneratedValue (strategy = AUTO)
+    private long id;
+    
+    
+    private double humidity; 
+            
+    private double temperature;
+    
+    private LocalDateTime time;
+    
+    public DataContainer(){
+        
+    }
+
+    /**
+     * @return the humidity
+     */
+    public double getHumidity() {
+        return humidity;
+    }
+
+    /**
+     * @param humidity the humidity to set
+     */
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    /**
+     * @return the temperature
+     */
+    public double getTemperature() {
+        return temperature;
+    }
+
+    /**
+     * @param temperature the temperature to set
+     */
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    /**
+     * @return the time
+     */
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    /**
+     * @param time the time to set
+     */
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+}
